0e922267f7e01e68f17d5caadb03c615896b2eaa
Kerstin Werner <32741758+Cathanys@users.noreply.github.com>
2023-02-18 11:58:10 +0100

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Better Bosses")]
 [assembly: AssemblyDescription("Valheim Mod to increase the health of bosses")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Azumatt")]
+[assembly: AssemblyCompany("NoobMods")]
 [assembly: AssemblyProduct("Better Bosses Mod")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
